f5fba52a08bb0bbca880950aec98b8e4391b672a
Sumit Bhimte <43449951+sumitbhimte@users.noreply.github.com>
2022-08-26 18:34:11 +0530

    Update Spirally traversing a matrix.cpp

--- Spirally traversing a matrix.cpp
+++ Spirally traversing a matrix.cpp
@@ -1,5 +1,17 @@
 // https://practice.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1
 
+/*
+| 1 | 2 | 3 | 4
+| 5 | 6 | 7 | 8
+| 9 |10 |11 | 12
+| 13|14 |15 | 16
+
+op = 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+
+*/
+
+// SOLUTION 1 :
+
 class Solution
 {   
     public: 
@@ -40,3 +52,48 @@ class Solution
         return v;
     }
 };
+
+
+//SOLUTION 2: 
+
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        int top =0;
+        int bottom = r;
+        int left = 0;
+        int right =c;
+        vector <int > v;
+        
+        while(top<bottom and left<right){
+            
+            for(int i=left;i<right;i++)  v.push_back(matrix[top][i]);
+            
+            top++;
+            
+            for(int i=top;i<bottom;i++)  v.push_back(matrix[i][right-1]);
+            
+            right--;
+            
+            if(top<bottom){
+                
+                for(int i=right-1;i>=left;i--)  v.push_back(matrix[bottom-1][i]);
+                bottom--;
+            }
+            
+            if(left<right){
+                
+                for(int i=bottom-1;i>=top;i--)  v.push_back(matrix[i][left]);
+                left++;
+            }
+            
+            
+        }
+        return v;
+    }
+};
+
+
