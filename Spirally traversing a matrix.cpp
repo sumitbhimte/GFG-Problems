abcf39382ded58bedd6f4dafbe2430bf84607322
Sumit Bhimte <43449951+sumitbhimte@users.noreply.github.com>
2022-08-26 19:55:45 +0530

    Updated 3 soln for different spiral model

--- Spirally traversing a matrix.cpp
+++ Spirally traversing a matrix.cpp
@@ -9,8 +9,50 @@
 op = 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
 
 */
+//SOLUTION 1: 
 
-// SOLUTION 1 :
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        int top =0;
+        int bottom = r;
+        int left = 0;
+        int right =c;
+        vector <int > v;
+        
+        while(top<bottom and left<right){
+            
+            for(int i=left;i<right;i++)  v.push_back(matrix[top][i]);
+            
+            top++;
+            
+            for(int i=top;i<bottom;i++)  v.push_back(matrix[i][right-1]);
+            
+            right--;
+            
+            if(top<bottom){
+                
+                for(int i=right-1;i>=left;i--)  v.push_back(matrix[bottom-1][i]);
+                bottom--;
+            }
+            
+            if(left<right){
+                
+                for(int i=bottom-1;i>=top;i--)  v.push_back(matrix[i][left]);
+                left++;
+            }
+            
+            
+        }
+        return v;
+    }
+};
+
+
+// SOLUTION 2 :
 
 class Solution
 {   
@@ -54,7 +96,17 @@ class Solution
 };
 
 
-//SOLUTION 2: 
+/*
+| 1 | 2 | 3 | 4
+| 5 | 6 | 7 | 8
+| 9 |10 |11 | 12
+| 13|14 |15 | 16
+
+op = 1 5 9 13 14 15 16 12 8 4 3 2 6 10 11 7
+
+*/
+
+// SOLUTION 3 :
 
 class Solution
 {   
@@ -62,38 +114,39 @@ class Solution
     //Function to return a list of integers denoting spiral traversal of matrix.
     vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
     {
-        int top =0;
-        int bottom = r;
+        int top = 0;
+        int btm = r -1;
         int left = 0;
-        int right =c;
+        int right = c -1;
+        
         vector <int > v;
+        
         
         while(top<bottom and left<right){
             
-            for(int i=left;i<right;i++)  v.push_back(matrix[top][i]);
+            for(int i=top;i<bottom;i++)  v.push_back(matrix[i][left]);
+            left++;
             
-            top++;
             
-            for(int i=top;i<bottom;i++)  v.push_back(matrix[i][right-1]);
-            
-            right--;
-            
-            if(top<bottom){
-                
-                for(int i=right-1;i>=left;i--)  v.push_back(matrix[bottom-1][i]);
-                bottom--;
-            }
+            for(int i=left;i<right;i++)  v.push_back(matrix[bottom-1][i]);
+            bottom--;
             
             if(left<right){
                 
-                for(int i=bottom-1;i>=top;i--)  v.push_back(matrix[i][left]);
-                left++;
+                for(int i=bottom-1;i>=top;i--)  v.push_back(matrix[i][right-1]);
+                right--;
             }
             
-            
+            if(top<bottom){
+                
+                for(int i=right-1;i>=left;i--)  v.push_back(matrix[top][i]);
+                top++;
+            }
+           
         }
         return v;
     }
 };
+
 
 
